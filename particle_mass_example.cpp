5c781aab5ae79706852da7a546ba29dc77ce77a9
Carlo de Falco <carlo.defalco@polimi.it>
2022-12-15 11:04:41 +0100

    add example with particles of different masses

---
+++ particle_mass_example.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <random>
+#include <quadgrid_cpp.h>
+#include <particles.h>
+
+
+using idx_t = quadgrid_t<std::vector<double>>::idx_t;
+
+int
+main (int argc, char *argv[]) {
+
+  quadgrid_t<std::vector<double>> grid;
+  grid.set_sizes (4, 16, 1./16., 1./16.);
+
+  constexpr idx_t num_particles = 100000;
+  particles_t ptcls (num_particles, {"label"}, {"m"}, grid);
+  std::cerr << " 1 " << "\n";
+
+  ptcls.dprops["m"].assign (num_particles, 1.);
+  std::cerr << " 2 " << "\n";
+
+
+  idx_t ilabel = 0;
+  std::iota (ptcls.iprops["label"].begin (), ptcls.iprops["label"].end (), ilabel);
+  std::cerr << " 3 " << "\n";
+
+
+  // for (auto ii : ptcls.x)
+  //    std::cout << ii << " ";
+  // std::cout << std::endl << std::endl;
+  
+  
+  //  for (auto icell = grid.begin_cell_sweep ();
+  //       icell != grid.end_cell_sweep (); ++icell) {
+
+
+  //    if (ptcls.grd_to_ptcl.count (icell->get_global_cell_idx ())) {      
+       
+  //      auto & plist = ptcls.grd_to_ptcl.at (icell->get_global_cell_idx ());
+  //      for (auto ii = 0; ii < plist.size (); ++ii) {
+  // 	auto jj = plist[ii];
+      
+  // 	if (ptcls.x[jj] > .5) {
+  // 	  ptcls.x[jj] = ptcls.x[jj] - .5;
+	  
+  // 	}
+  //      }
+  //    }
+  //  }
+
+  //  for (auto ii : ptcls.x)
+  //    std::cout << ii << " ";
+  //  std::cout << std::endl << std::endl;
+   for (auto & icell : ptcls.grd_to_ptcl) {
+    auto cellnum = icell.first;
+    auto & plist = icell.second;
+    for (auto jj : plist) {
+      if (ptcls.x[jj] > .5) {
+  	//std::cerr << " jj = " << jj << " ptcls.x[jj] = " << ptcls.x[jj];
+  	ptcls.dprops["m"][jj] = .0;
+  	//std::cerr << " ptcls.x[jj] = " << ptcls.x[jj] << std::endl;
+      }      
+    }
+  }
+   
+  ptcls.init_particle_mesh ();
+
+  std::cerr << " 4 " << "\n";
+
+  std::map<std::string, std::vector<double>>
+    vars{{"m", std::vector<double>(grid.num_global_nodes (), 0.0)}};
+  std::cerr << " 5 " << "\n";
+
+  ptcls.p2g (vars, true);
+  std::cerr << " 6 " << "\n";
+  
+  for (auto ii : vars["m"])
+    std::cout << ii << std::endl;
+
+  
+  std::cerr << " 7 " << "\n";
+  
+  return 0;
+};
+
+
