ce38e20d06ec57d3c2f9e019e5716dc7c8b5be01
Carlo de Falco <carlo.defalco@polimi.it>
2022-12-21 11:06:42 +0100

    fix vertex ordering

--- particle_mass_example.cpp
+++ particle_mass_example.cpp
@@ -10,9 +10,9 @@ int
 main (int argc, char *argv[]) {
 
   quadgrid_t<std::vector<double>> grid;
-  grid.set_sizes (40, 160, 1./160., 1./160.);
+  grid.set_sizes (4, 40, 1./40., 1./16.);
 
-  constexpr idx_t num_particles = 1000000;
+  constexpr idx_t num_particles = 50000000;
   particles_t ptcls (num_particles, {"label"}, {"m","vx","vy"}, grid);
   std::cerr << " 1 " << "\n";
 
@@ -28,17 +28,17 @@ main (int argc, char *argv[]) {
   // for (auto ii : ptcls.x)
   //    std::cout << ii << " ";
   // std::cout << std::endl << std::endl;
-  
-  
+
+
   //  for (auto icell = grid.begin_cell_sweep ();
   //       icell != grid.end_cell_sweep (); ++icell) {
-  //    if (ptcls.grd_to_ptcl.count (icell->get_global_cell_idx ())) {             
+  //    if (ptcls.grd_to_ptcl.count (icell->get_global_cell_idx ())) {
   //      auto & plist = ptcls.grd_to_ptcl.at (icell->get_global_cell_idx ());
   //      for (auto ii = 0; ii < plist.size (); ++ii) {
-  // 	auto jj = plist[ii];
-  // 	if (ptcls.x[jj] > .5) {
-  // 	  ptcls.x[jj] = ptcls.x[jj] - .5;
-  // 	}
+  //	auto jj = plist[ii];
+  //	if (ptcls.x[jj] > .5) {
+  //	  ptcls.x[jj] = ptcls.x[jj] - .5;
+  //	}
   //    }
   //   }
   //  }
@@ -47,23 +47,23 @@ main (int argc, char *argv[]) {
   //  std::cout << std::endl << std::endl;
 
 
-  
+
   // for (auto & icell : ptcls.grd_to_ptcl) {
   //   auto cellnum = icell.first;
   //   auto & plist = icell.second;
   //   for (auto jj : plist) {
   //     if (ptcls.x[jj] > .5) {
-  // 	//std::cerr << " jj = " << jj << " ptcls.x[jj] = " << ptcls.x[jj];
-  // 	ptcls.dprops["m"][jj] = .0;
-  // 	//std::cerr << " ptcls.x[jj] = " << ptcls.x[jj] << std::endl;
-  //     }      
+  //	//std::cerr << " jj = " << jj << " ptcls.x[jj] = " << ptcls.x[jj];
+  //	ptcls.dprops["m"][jj] = .0;
+  //	//std::cerr << " ptcls.x[jj] = " << ptcls.x[jj] << std::endl;
+  //     }
   //   }
   // }
 
-  for (idx_t ip = 0; ip < ptcls.x.size (); ++ip) {    
+  for (idx_t ip = 0; ip < ptcls.x.size (); ++ip) {
     if (ptcls.x[ip] > .5) {
-      ptcls.dprops["m"][ip] = .0;
-    }      
+      ptcls.dprops["m"][ip] *= .1;
+    }
   }
 
 
@@ -72,20 +72,20 @@ main (int argc, char *argv[]) {
   std::cerr << " 4 " << "\n";
 
   std::map<std::string, std::vector<double>>
-    vars{{"m", std::vector<double>(grid.num_global_nodes (), 0.0)}};
+    vars{{"m", std::vector<double>(grid.num_global_nodes (), 0.)}};
   std::cerr << " 5 " << "\n";
 
   std::vector<std::string> pvarnames{"m"};
   ptcls.p2g (vars, pvarnames, pvarnames, true);
   std::cerr << " 6 " << "\n";
-  
+
   //  for (auto ii : vars["m"])
   //  std::cout << ii << std::endl;
 
   grid.vtk_export ("particles_mass_example.vts", vars);
-  
+
   std::cerr << " 7 " << "\n";
-  
+
   return 0;
 };
 
