ba4a5148d42b3d25e1f0223cdeb1569db1167bc5
Carlo de Falco <carlo.defalco@polimi.it>
2022-12-21 07:03:58 +0100

    output of particle variables

--- particles.h
+++ particles.h
@@ -2,11 +2,14 @@
 #define PARTICLES_H
 
 #include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <quadgrid_cpp.h>
 #include <random>
 #include <string>
-#include <quadgrid_cpp.h>
-#include <map>
-#include <iostream>
+
+
 
 struct
 particles_t {
@@ -21,6 +24,100 @@ particles_t {
   std::vector<double> M;
   std::map<idx_t, std::vector<idx_t>> grd_to_ptcl;
   const quadgrid_t<std::vector<double>>& grid;
+
+  enum class
+  output_format : idx_t {
+    csv = 0,
+    octave_ascii = 1
+  };
+
+  template<output_format fmt = output_format::csv>
+  void
+  print (std::ostream & os) const {
+    os << "output format not implementd" << std::endl;
+  };
+
+  template<>
+  void
+  print<output_format::csv> (std::ostream & os) const {
+    os << "\"x\", " << "\"y\"";
+    for (auto const & ii : dprops)
+      os << ", \"" << ii.first << "\"";
+    for (auto const & ii : iprops)
+      os << ", \"" << ii.first << "\"";
+    os << std::endl;
+
+
+    for (idx_t jj = 0; jj < x.size (); ++jj) {
+      os << x[jj] << ", " << y[jj];
+      for (auto const & ii : dprops)
+	os << ", " << std::setprecision (16) << ii.second[jj];
+      for (auto const & ii : iprops)
+	os << ", " << ii.second[jj];
+      os << std::endl;
+    }
+  };
+
+  template<>
+  void
+  print<output_format::octave_ascii> (std::ostream & os) const {
+
+
+    os << "# name: x" << std::endl
+       << "# type: matrix" << std::endl
+       << "# rows: 1" << std::endl
+       << "# columns: " << x.size () << std::endl;
+    for (auto const & kk : x) {
+      os  << std::setprecision(16) << kk << " ";
+    }
+    os << std::endl;
+
+    os << "# name: y" << std::endl
+       << "# type: matrix" << std::endl
+       << "# rows: 1" << std::endl
+       << "# columns: " << y.size () << std::endl;
+    for (auto const & kk : y) {
+      os  << std::setprecision(16) << kk << " ";
+    }
+    os << std::endl;
+
+    os << "# name: dprops" << std::endl
+       << "# type: scalar struct" << std::endl
+       << "# ndims: 2" << std::endl
+       << "1 1" << std::endl
+       << "# length: " << dprops.size () << std::endl;
+
+
+    for (auto const & ii : dprops) {
+      os << "# name: " << ii.first << std::endl
+	 << "# type: matrix" << std::endl
+	 << "# rows: 1" << std::endl
+	 << "# columns: " << ii.second.size () << std::endl;
+      for (auto const & kk : ii.second) {
+	os << std::setprecision(16) << kk << " ";
+      }
+      os << std::endl;
+    }
+    os << std::endl;
+
+    os << "# name: iprops" << std::endl
+       << "# type: scalar struct" << std::endl
+       << "# ndims: 2" << std::endl
+       << "1 1" << std::endl
+       << "# length: " << iprops.size () << std::endl;
+
+    for (auto const & ii : iprops) {
+      os << "# name: " << ii.first << std::endl
+	 << "# type: int64 matrix" << std::endl
+	 << "# ndims: 2" << std::endl
+	 << "1 " << ii.second.size () << std::endl;
+      for (auto const & kk : ii.second) {
+	os << kk << " ";
+      }
+      os << std::endl;
+    }
+    os << std::endl;
+  };
 
   particles_t (idx_t n, const std::vector<std::string>& ipropnames,
 	       const std::vector<std::string>& dpropnames,
@@ -103,11 +200,11 @@ particles_t {
 
   template<typename GT, typename PT>
   void
-  p2g (std::map<std::string, std::vector<double>> & vars,       
+  p2g (std::map<std::string, std::vector<double>> & vars,
        PT const & pvarnames,
        GT const & gvarnames,
        bool apply_mass = false) const {
-    
+
     double N = 0.0, xx = 0.0, yy = 0.0;
     idx_t idx = 0;
 
@@ -124,7 +221,7 @@ particles_t {
 
 	  for (idx_t inode = 0; inode < 4; ++inode) {
 	    N = icell->shp(xx, yy, inode);
-	    for (std::size_t ivar = 0; ivar < gvarnames.size (); ++ivar) {	    
+	    for (std::size_t ivar = 0; ivar < gvarnames.size (); ++ivar) {
 	      vars[getkey(gvarnames, ivar)][icell->gt(inode)]  +=
 		N * dprops.at (getkey(pvarnames, ivar))[idx];
 	    }
@@ -146,13 +243,13 @@ particles_t {
 	GT const & gxvarnames,
 	GT const & gyvarnames,
 	bool apply_mass = false) const {
-    
+
     double xx = 0.0, yy = 0.0, Nx = 0.0, Ny = 0.0;
     idx_t idx = 0;
 
     for (auto icell = grid.begin_cell_sweep();
 	 icell != grid.end_cell_sweep(); ++icell) {
-      
+
       if (grd_to_ptcl.count (icell->get_global_cell_idx ()) > 0)
 	for (idx_t ii = 0;
 	     ii < grd_to_ptcl.at (icell->get_global_cell_idx ()).size ();
@@ -167,12 +264,12 @@ particles_t {
 	    Nx = icell->shg (xx, yy, 0, inode);
 	    Ny = icell->shg (xx, yy, 1, inode);
 
-	    for (std::size_t ivar = 0; ivar < pvarnames.size (); ++ivar) {	    
+	    for (std::size_t ivar = 0; ivar < pvarnames.size (); ++ivar) {
 	      vars[getkey(gxvarnames, ivar)][icell->gt(inode)]  +=
 		Nx * dprops.at (getkey(pvarnames, ivar))[idx];
 	      vars[getkey(gyvarnames, ivar)][icell->gt(inode)]  +=
 		Ny * dprops.at (getkey(pvarnames, ivar))[idx];
-	    }	    
+	    }
 	  }
 	}
 
@@ -185,7 +282,7 @@ particles_t {
 	  vars[getkey(gyvarnames, ivar)][ii]  /= M[ii];
 	}
 
-  }; 
+  };
 
   void
   g2p (const std::map<std::string, std::vector<double>>& vars,
@@ -199,34 +296,35 @@ particles_t {
        GT const & gvarnames,
        PT const & pvarnames,
        bool apply_mass) {
- 
+
     double N = 0.0, xx = 0.0, yy = 0.0;
     idx_t idx = 0;
 
     for (auto icell = grid.begin_cell_sweep ();
-         icell != grid.end_cell_sweep (); ++icell) {
-      
+	 icell != grid.end_cell_sweep (); ++icell) {
+
       if (grd_to_ptcl.count (icell->get_global_cell_idx ()) > 0)
-        for (idx_t ii = 0;
-             ii < grd_to_ptcl.at (icell->get_global_cell_idx ()).size ();
-             ++ii) {
+	for (idx_t ii = 0;
+	     ii < grd_to_ptcl.at (icell->get_global_cell_idx ()).size ();
+	     ++ii) {
 
 	  idx = grd_to_ptcl.at(icell->get_global_cell_idx ())[ii];
-          xx = x[idx];
-          yy = y[idx];
+	  xx = x[idx];
+	  yy = y[idx];
 
-          for (idx_t inode = 0; inode < 4; ++inode) {
-            N = apply_mass ?
+	  for (idx_t inode = 0; inode < 4; ++inode) {
+	    N = apply_mass ?
 	      icell->shp(xx, yy, inode) * M[icell->gt(inode)] :
 	      icell->shp(xx, yy, inode);
-            for (std::size_t ivar = 0; ivar < gvarnames.size (); ++ivar)
-              dprops.at (getkey (pvarnames, ivar))[idx] += N * vars.at (getkey (gvarnames, ivar))[icell->gt(inode)];
-          }
-        }
+	    for (std::size_t ivar = 0; ivar < gvarnames.size (); ++ivar)
+	      dprops.at (getkey (pvarnames, ivar))[idx] += N * vars.at (getkey (gvarnames, ivar))[icell->gt(inode)];
+	  }
+	}
 
     }
 
   };
+
 
 };
 
