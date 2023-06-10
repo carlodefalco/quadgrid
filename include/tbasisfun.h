42e7dc813eca43719735b81f01c2da4862b43de6
Carlo de Falco <carlo.defalco@polimi.it>
2023-06-10 19:04:57 +0200

    knot vvector example

--- include/tbasisfun.h
+++ include/tbasisfun.h
@@ -35,59 +35,61 @@ namespace bspline {
     const FLT Umin = *(std::min_element (Ubegin, Uend));
     const FLT Umax = *(std::max_element (Ubegin, Uend));
 
-    if ((u <= Umin) || ( u > Umax)) {
-      N = 0.0;
-      return N;
-    } else if (p == 0) {
-      N = 1.0;
-      return N;
-    } else if (p == 1) {
-      if (u < *std::next(Ubegin, 1)) {
-	N = ratio ((u - *Ubegin),
-		   (*std::next(Ubegin, 1) - *Ubegin));
-	return N;
-      } else {
-	N = ratio ((*std::next(Ubegin, 2) - u),
-		   (*std::next(Ubegin, 2) - *std::next(Ubegin, 1)));
-	return N;
-      }
-    } else if (p == 2) {
-      const FLT ln = u - *Ubegin;
-      const FLT dn = *std::next(Ubegin, 3) - u;
-      const FLT ld = *std::next(Ubegin, 2) - *Ubegin; 
-      const FLT dd = *std::next(Ubegin, 3) - *std::next(Ubegin, 1);
-      if (u < *std::next(Ubegin, 1)) {
-	N = ratio (ln*ln, (ld * (*std::next(Ubegin, 1) - *Ubegin)));
-	return N;
-      } else if (u > *std::next(Ubegin, 2)) {
-	N = ratio (dn*dn,
-		   (dd * (*std::next(Ubegin, 3) - *std::next(Ubegin, 2))));
-	return N;
-      } else {
-	if (ld > FLT(0.0)) {
-	  N += ratio (ln * (*std::next(Ubegin, 2) - u),
-		      ((*std::next(Ubegin, 2) - *std::next(Ubegin, 1)) * ld));
-	}
-	if (dd > FLT(0.0)) {
-	  N += ratio (dn * (u - *std::next(Ubegin, 1)),
-		      ((*std::next(Ubegin, 2) - *std::next(Ubegin, 1)) * dd));
-	}
-	return N;
-      }
-    }
+    if (u >= Umin && u <= Umax) {
 
-    const FLT ln = u - *Ubegin;
-    const FLT ld = *std::next(Uend, - 2) - *Ubegin;
-    if (ld > FLT(0.0)) {
-      N += ln * onebasisfun (u, p-1, Ubegin, std::next(Uend, - 1)) / ld; 
-    }
+      if (p == 0) {
+	N = 1.0;
+      }
+      
+      else if (p == 1) {
+	if (u < *std::next(Ubegin, 1)) {
+	  N = ratio ((u - *Ubegin),
+		     (*std::next(Ubegin) - *Ubegin));
+	}
+	else {
+	  N = ratio ((*std::next(Ubegin, 2) - u),
+		     (*std::next(Ubegin, 2) - *std::next(Ubegin)));
+	}
+      }
+      
+      else if (p == 2) {
+	const FLT ln = u - *Ubegin;
+	const FLT dn = *std::next(Ubegin, 3) - u;
+	const FLT ld = *std::next(Ubegin, 2) - *Ubegin; 
+	const FLT dd = *std::next(Ubegin, 3) - *std::next(Ubegin, 1);
+	if (u < *std::next(Ubegin, 1)) {
+	  N = ratio (ln*ln, (ld * (*std::next(Ubegin, 1) - *Ubegin)));
+	}
+	else if (u > *std::next(Ubegin, 2)) {
+	  N = ratio (dn*dn,
+		     (dd * (*std::next(Ubegin, 3) - *std::next(Ubegin, 2))));
+	}
+	else {
+	  if (ld > FLT(0.0)) {
+	    N += ratio (ln * (*std::next(Ubegin, 2) - u),
+			((*std::next(Ubegin, 2) - *std::next(Ubegin, 1)) * ld));
+	  }
+	  if (dd > FLT(0.0)) {
+	    N += ratio (dn * (u - *std::next(Ubegin, 1)),
+			((*std::next(Ubegin, 2) - *std::next(Ubegin, 1)) * dd));
+	  }
+	}
+      }
+
+      else {
+	const FLT ln = u - *Ubegin;
+	const FLT ld = *std::next(Uend, - 2) - *Ubegin;
+	if (ld > FLT(0.0)) {
+	  N += ln * onebasisfun (u, p-1, Ubegin, std::next(Uend, - 1)) / ld; 
+	}
   
-    const FLT dn = *std::next (Uend, - 1) - u;
-    const FLT dd = *std::next (Uend, - 1) - *std::next (Ubegin , 1);
-    if (dd > FLT(0.0)) {
-      N += dn * onebasisfun (u, p-1, std::next (Ubegin, 1), Uend) / dd;
+	const FLT dn = *std::next (Uend, - 1) - u;
+	const FLT dd = *std::next (Uend, - 1) - *std::next (Ubegin , 1);
+	if (dd > FLT(0.0)) {
+	  N += dn * onebasisfun (u, p-1, std::next (Ubegin, 1), Uend) / dd;
+	}
+      }
     }
-  
     return N;
   };
 
@@ -106,29 +108,55 @@ namespace bspline {
     const FLT Umin = *(std::min_element (Ubegin, Uend));
     const FLT Umax = *(std::max_element (Ubegin, Uend));
     
-    if ((u <= Umin) || ( u > Umax)) {
-      Nder = FLT(0.0);
-      return Nder;
-    } else if (p == 0) {
-      Nder = FLT(0.0);
-      return Nder;
-    } else {    
+    if ((u >= Umin) && ( u <= Umax)) {
+   
+      if (p == 0) {
+	Nder = FLT(0.0);
+      }
+      else {    
 
-      const FLT ld = *std::next (Uend, -2) - *Ubegin;
-      if (ld != FLT(0.0)) {
-	Nder += p * onebasisfun (u, p-1, Ubegin, std::next (Uend, -1)) / ld;
+	const FLT ld = *std::next (Uend, -2) - *Ubegin;
+	if (std::abs (ld) > FLT(0.0)) {
+	  Nder += ratio (p * onebasisfun (u, p-1, Ubegin, std::next (Uend, -1)), ld);
+	}
+	
+	const FLT dd = *std::next (Uend, -1) - *std::next (Ubegin, 1);
+	if (std::abs (dd) > FLT(0.0)) { 
+	  Nder -= ratio (p * onebasisfun (u, p-1, std::next (Ubegin, 1), Uend), dd);
+	}
+	
       }
-    
-      const FLT dd = *std::next (Uend, -1) - *std::next (Ubegin, 1);
-      if (dd != FLT(0.0)) { 
-	Nder -= p * onebasisfun (u, p-1, std::next (Ubegin, 1), Uend) / dd;
-      }
-      
     }
 
     return Nder;
   };
 
+   //! \brief create an uniform open knot vector given the list
+  //! of breaks, a (scalar) degree p and a (scalar) regularity r.
+
+  //! @param bb begin of list of breaks.
+  //! @param be end of list of breaks. 
+  //! @param p basis function degree.
+  //! @param r basis functions regularity.
+  //! @return k knoct vector.
+
+  template<typename INT, typename ITERATOR, typename FLT=double>
+  std::vector<FLT>
+  open_knot_vector (ITERATOR const bb, ITERATOR const be, INT const p, INT const r) {
+
+    const INT Nb = std::distance (bb, be);
+    const INT Nk = (Nb - 2) + 2 * (p + 1);
+
+    std::vector<FLT> k (Nk, *bb);
+
+    auto bi = std::next (bb);
+    auto ki = std::copy (bi, be, std::next (k.begin (), p + 1));
+    std::fill (ki, k.end (), *(std::next (be, -1)));
+
+    return k;
+  };
+
+  
   template<typename INT, typename FLT, typename ITERATOR>
   FLT
   onebasisfun2d (FLT const u, FLT const v, INT const pu, INT const pv,
