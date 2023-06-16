d1750bb627c3d75c0730d4853e2cf86f72dd1366
Carlo de Falco <carlo.defalco@polimi.it>
2023-06-16 10:56:03 +0200

    add test to show (and help fix) bug in tbasisfun

--- test/test_tbasisfun_bug.cpp
+++ test/test_tbasisfun_bug.cpp
@@ -8,34 +8,23 @@ using namespace bspline;
 int main(int argc, char *argv[]) {
 
   int p{2};
-  constexpr auto N = 100;
+
+  // This is a knot vector with a knot of multiplicity p
+  // so that ath the corresponding break the funcion is C0
+  // and interpolating.
+  std::vector<double> k{1.0, 2.0, 2.0, 3.0};
   
-  std::vector<double> k{1.0, 1.99, 2.01, 3.0};
   auto kb = k.begin ();
   auto ke = k.end ();
 
-  /*
-  std::vector<double> x(N+1, 0.0);
-  std::vector<double> y(N+1, 0.0);
-
-  x[0] = *kb;
-  y[0] = onebasisfun (x[0], p, kb, ke);
-  double dx = (*(std::prev (ke)) - *kb) / double (N);
-  for (int ii = 1; ii < N+1; ++ii) {
-    x[ii] = x[ii-1] + dx; 
-    if (x[ii] >= (*(std::prev (ke)) - *(std::prev (ke)) * 4.0 * std::numeric_limits<double>::epsilon ()))
-      x[ii] = *(std::prev (ke));
-    y[ii] = onebasisfun (x[ii], p, kb, ke);
-  }
-
-  for (int ii = 0; ii < N+1; ++ii) {
-    std::cout  << std::setprecision (19) << x[ii] << "   " << y[ii] << std::endl;
-  }
-  */
-
+  // Evaluate the basis function EXACTLY at the break point
   double x{2.0};
   double y  = onebasisfun (x, p, kb, ke);
-  std::cout  << std::setprecision (19) << x << "   " << y << std::endl;
+
+  // The value of the basis function should be 1.0, but is 2.0 if the
+  // code does not understand that there is no knot span between two
+  // identical knots.
+  std::cout  << std::setprecision (19) << "x = (should be 2.0) " << x << " y = (should be 1.0)  " << y << std::endl;
   
   return 0;  
 }
