34c922b63bc64f477ad4de43d175a3be229f3f57
Carlo de Falco <carlo.defalco@polimi.it>
2023-10-06 15:03:03 +0200

    small optimization

--- include/particles_imp.h
+++ include/particles_imp.h
@@ -204,14 +204,13 @@ particles_t::g2pd
   idx_t idx = 0;
 
   for (std::size_t ivar = 0; ivar < std::size (gvarnames); ++ivar) {
-    
+    auto const & gvar = vars.at (getkey (gvarnames, ivar));
+    auto & dpropx = dprops.at (getkey (pxvarnames, ivar));
+    auto & dpropy = dprops.at (getkey (pyvarnames, ivar));
+
     for (auto icell = grid.begin_cell_sweep ();
 	 icell != grid.end_cell_sweep (); ++icell) {
       if (grd_to_ptcl.count (icell->get_global_cell_idx ()) > 0) {
-	auto & dpropx = dprops.at (getkey (pxvarnames, ivar));
-	auto & dpropy = dprops.at (getkey (pyvarnames, ivar));
-	auto const & gvar = vars.at (getkey (gvarnames, ivar));
-
 	for (idx_t ii = 0;
 	     ii < grd_to_ptcl.at (icell->get_global_cell_idx ()).size ();
 	     ++ii) {
