2a45b33bb9b27b04b4d6ab753fce4952944482b0
Carlo de Falco <carlo.defalco@polimi.it>
2023-10-13 22:12:39 +0200

    reverse index lookup

--- src/particles.cpp
+++ src/particles.cpp
@@ -102,13 +102,17 @@ void
 particles_t::init_particle_mesh () {
 
   for (auto & igrd : grd_to_ptcl)
-    std::vector<idx_t>{}. swap (igrd.second);
+    igrd.second.clear ();
 
-  for (auto ii = 0; ii < x.size (); ++ii) {
+  ptcl_to_grd.fill (this->num_particles, 0);
+    
+  ptcl_to_grd
+  for (auto ii = 0; ii < this->num_particles; ++ii) {
     idx_t c = static_cast<idx_t> (std::floor (x[ii] / grid.hx ()));
     idx_t r = static_cast<idx_t> (std::floor (y[ii] / grid.hy ()));
 
     grd_to_ptcl[grid.sub2gind (r, c)].push_back (ii);
+    ptcl_to_grd[ii]=grid.sub2gind (r, c);
   }
 }
 
